115018ea54fd7c1769e306bdfad931eeaa42f3d8
BorisLechev <b.k.lechev@gmail.com>
2022-02-22 23:00:59 +0200

    Add MyForum.Data.Common class library.

---
+++ Server/Data/MyForum.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyForum.Data.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyForum.Data.Common.Repositories
+{
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        Task<TEntity> GetByIdWithDeletedAsync(params object[] id);
+
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        void HardDelete(TEntity entity);
+
+        void HardDeleteRange(IEnumerable<TEntity> entities);
+
+        void Undelete(TEntity entity);
+    }
+}
