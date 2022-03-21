dc30e925d6ddf4ad7b8401a77c4e823e5fabc6f2
BorisLechev <b.k.lechev@gmail.com>
2022-03-21 17:26:10 +0200

    Redirect to Login page if unauthorized user try to access Create Article page.

--- Client/src/pages/create-article/create-article.js
+++ Client/src/pages/create-article/create-article.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import PageLayout from "../layout/layout";
 import ArticleInputForm from "../../components/forms/article-input/article-input";
 import articlesService from "../../services/articles";
+import UserContext from "../../utils/context";
 
 const CreateArticlePage = () => {
     const navigate = useNavigate();
+    const context = useContext(UserContext);
 
     const handleFormSubmit = async (event, title, content) => {
         event.preventDefault();
@@ -18,18 +20,24 @@ const CreateArticlePage = () => {
         await articlesService.createArticle(
             body,
             (response) => navigate(`/articles/${response.id}`),
-            (error) => console.log(error),
+            () => console.log(),
         );
     };
 
     return (
-        <PageLayout>
-            <ArticleInputForm
-                initialTitle=""
-                initialContent=""
-                handleFormSubmit={handleFormSubmit}
-            />
-        </PageLayout>
+        <React.Fragment>
+            {context.user.username ? (
+                <PageLayout>
+                    <ArticleInputForm
+                        initialTitle=""
+                        initialContent=""
+                        handleFormSubmit={handleFormSubmit}
+                    />
+                </PageLayout>
+            ) : (
+                <Navigate to="/login" replace />
+            )}
+        </React.Fragment>
     );
 };
 
