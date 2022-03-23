2af7db0a9733a13dd1005248fbc3e78812c9aab3
BorisLechev <b.k.lechev@gmail.com>
2022-03-23 16:30:26 +0200

    Add Result class with implicit operators.

---
+++ Server/Services/MyForum.Services.Data/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace MyForum.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Result
+    {
+        public bool Succeeded { get; private set; }
+
+        public bool Failure => !this.Succeeded;
+
+        public string Error { get; private set; }
+
+        public static implicit operator Result(bool succeeded)
+            => new Result
+            {
+                Succeeded = succeeded,
+            };
+
+        public static implicit operator Result(string error)
+            => new Result
+            {
+                Succeeded = false,
+                Error = error,
+            };
+    }
+}
